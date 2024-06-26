2a1b96d6455e6d2ee69603d1d3b5992bb0a592b1
Aniket Kumar <120406903+Aniket4548@users.noreply.github.com>
2024-06-26 16:21:29 +0530

    Update maps.py

--- maps.py
+++ maps.py
@@ -14,9 +14,9 @@ except Exception as e:
 
 
 try:
-location_info = {'location': 'Delhi, India'}
-map1 = prettymaps.plot(location_info)
-st.pyplot(map1)  # Display the map
+  location_info = {'location': 'Delhi, India'}
+  map1 = prettymaps.plot(location_info)
+  st.pyplot(map1)  # Display the map
 
 except Exception as e:
   st.error(f"Error generating map: {e}")
